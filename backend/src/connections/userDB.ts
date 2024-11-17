311b72b0522428945a9cf238d7db802bc1d414be
root <root@DESKTOP-4TQ3HH9>
2024-11-17 12:00:08 -0800

    Code quality & Club backend

--- backend/src/connections/userDB.ts
+++ backend/src/connections/userDB.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const connectUserDB = async () => {
-  console.log(process.env.MONGODB_URI);
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+	console.log(process.env.MONGODB_URI);
+	try {
+		const conn = await mongoose.connect(process.env.MONGODB_URI || "");
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error: any) {
+		console.error(`Error: ${error.message}`);
+		process.exit(1);
+	}
 };
 
 export default connectUserDB;
