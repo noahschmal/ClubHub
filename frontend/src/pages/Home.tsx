35e91f01fd771caea60a0d648f1077da258d8708
Noah Schmalenberger <noah.schmalenberger@wsu.edu>
2024-11-17 20:00:43 -0800

    removed db connect info from home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,38 +8,38 @@ const Home = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
-  const userProfileInfo = useAppSelector((state) => state.auth.userProfileData);
+  // const basicUserInfo = useAppSelector((state) => state.auth.basicUserInfo);
+  // const userProfileInfo = useAppSelector((state) => state.auth.userProfileData);
 
-  useEffect(() => {
-    if (basicUserInfo) {
-      dispatch(getUser(basicUserInfo.id));
-    }
-  }, [basicUserInfo]);
+  // useEffect(() => {
+  //   if (basicUserInfo) {
+  //     dispatch(getUser(basicUserInfo.id));
+  //   }
+  // }, [basicUserInfo]);
 
-  const handleLogout = async () => {
-    try {
-      await dispatch(logout()).unwrap();
-      navigate("/login");
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await dispatch(logout()).unwrap();
+  //     navigate("/login");
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
   return (
     <>
       <h1>Home</h1>
-      <h4>Name: {userProfileInfo?.name}</h4>
-      <h4>Email: {userProfileInfo?.email}</h4>
+      {/* <h4>Name: {userProfileInfo?.name}</h4>
+      <h4>Email: {userProfileInfo?.email}</h4> */}
       
       <input type="text"></input>
 
-      <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogout}>
+      {/* <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogout}>
         Create Club
       </Button>
       <Button variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleLogout}>
         Logout
-      </Button>
+      </Button> */}
     </>
   );
 };
